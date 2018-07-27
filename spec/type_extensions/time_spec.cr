cd2204b6e114c69e44b5abbd65d2c9aa8cb3d5f3
Edward Loveall <edward@thoughtbot.com>
2018-07-27 13:01:42 -0400

    Add tests for time extension

---
+++ spec/type_extensions/time_spec.cr
@@ -0,0 +1,29 @@
+require "../../spec_helper"
+
+describe "Time column type" do
+  describe ".parse" do
+    it "casts an ISO8601 String successfully" do
+      time = Time.new.to_s("%FT%X%z")
+
+      result = Time::Lucky.parse(time)
+
+      result.should be_a(LuckyRecord::Type::SuccessfulCast(Time))
+    end
+
+    it "casts a Time successfully" do
+      time = Time.new
+
+      result = Time::Lucky.parse(time)
+
+      result.value.should eq(time)
+    end
+
+    it "can't cast an invalid value" do
+      time = Time.new
+
+      result = Time::Lucky.parse("oh no")
+
+      result.should be_a(LuckyRecord::Type::FailedCast)
+    end
+  end
+end
