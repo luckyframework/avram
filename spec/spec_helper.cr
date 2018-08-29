e7e5c5f9bbe5de0dfa27cc0e5da28b972fafd10c
Paul Smith <paulcsmith0218@gmail.com>
2018-08-29 10:19:28 -0400

    Run create in Travis

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,17 +3,6 @@ require "../src/lucky_record"
 require "./support/**"
 require "../config/database"
 
-LuckyRecord::Repo.configure do |settings|
-  if ENV["DATABASE_URL"]?
-    settings.url = ENV["DATABASE_URL"]
-  else
-    settings.url = LuckyRecord::PostgresURL.build(
-      database: "lucky_record_test",
-      hostname: "localhost"
-    )
-  end
-end
-
 Spec.before_each do
   LuckyRecord::Repo.truncate
 end
