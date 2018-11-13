5169af4664c8c9dd80e871bdffaf2214083a0f25
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-11-13 22:21:40 +0100

    Allow adding UUID columns (that are not associations)
    Adding a simple UUID column failed with "undefined method add_column"
    because the UUID type was not recognized for default values.

--- spec/migrator/alter_table_statement_spec.cr
+++ spec/migrator/alter_table_statement_spec.cr
@@ -13,6 +13,7 @@ describe Avram::Migrator::AlterTableStatement do
       add joined_at : Time, default: :now
       add updated_at : Time, fill_existing_with: :now
       add future_time : Time, default: Time.new
+      add new_id : UUID, default: UUID.new("46d9b2f0-0718-4d4c-a5a1-5af81d5b11e0")
       remove :old_field
       remove_belongs_to :employee
     end
@@ -30,6 +31,7 @@ describe Avram::Migrator::AlterTableStatement do
       ADD joined_at timestamptz NOT NULL DEFAULT NOW(),
       ADD updated_at timestamptz,
       ADD future_time timestamptz NOT NULL DEFAULT '#{Time.new.to_utc}',
+      ADD new_id uuid NOT NULL DEFAULT '46d9b2f0-0718-4d4c-a5a1-5af81d5b11e0',
       DROP old_field,
       DROP employee_id
     SQL
