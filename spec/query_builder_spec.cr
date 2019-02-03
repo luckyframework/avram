luckyframework/lucky_record

Add `where` clauses for checking if `NULL`

spec/query_builder_spec.cr
@@ -22,8 +22,9 @@ describe "LuckyRecord::QueryBuilder" do
     query = new_query
       .where(LuckyRecord::Where::Equal.new(:name, "Paul"))
       .where(LuckyRecord::Where::GreaterThan.new(:age, "20"))
+      .where(LuckyRecord::Where::Null.new(:nickname))
       .limit(1)
-    query.statement.should eq "SELECT * FROM users WHERE name = $1 AND age > $2 LIMIT 1"
+    query.statement.should eq "SELECT * FROM users WHERE name = $1 AND age > $2 AND nickname IS NULL LIMIT 1"
     query.args.should eq ["Paul", "20"]
   end
 

spec/where_spec.cr
@@ -1,9 +1,15 @@
 require "./spec_helper"
 
 macro should_negate(original_where, expected_negated_where)
-  clause = {{original_where}}.new("column", "value").negated
+  {% if original_where.resolve < LuckyRecord::Where::ComparativeSqlClause %}
+    clause = {{original_where}}.new("column", "value").negated
+  {% else %}
+    clause = {{original_where}}.new("column").negated
+  {% end %}
   clause.column.should eq "column"
-  clause.value.should eq "value"
+  {% if original_where.resolve < LuckyRecord::Where::ComparativeSqlClause %}
+    clause.value.should eq "value"
+  {% end %}
   clause.should be_a({{expected_negated_where}})
 end
 
@@ -21,5 +27,6 @@ describe LuckyRecord::Where do
     should_negate(LuckyRecord::Where::NotIlike, LuckyRecord::Where::Ilike)
     should_negate(LuckyRecord::Where::In, LuckyRecord::Where::NotIn)
     should_negate(LuckyRecord::Where::NotIn, LuckyRecord::Where::In)
+    should_negate(LuckyRecord::Where::Null, LuckyRecord::Where::NotNull)
   end
 end

src/lucky_record/query_builder.cr
@@ -210,7 +210,13 @@ class LuckyRecord::QueryBuilder
 
   private def joined_wheres_queries
     if @wheres.any? || @raw_wheres.any?
-      statements = @wheres.map(&.prepare(next_prepared_statement_placeholder))
+      statements = @wheres.map do |sql_clause|
+        if sql_clause.is_a?(LuckyRecord::Where::ComparativeSqlClause)
+          sql_clause.prepare(next_prepared_statement_placeholder)
+        else
+          sql_clause.prepare
+        end
+      end
       statements += @raw_wheres.map(&.to_sql)
 
       "WHERE " + statements.join(" AND ")
@@ -218,7 +224,9 @@ class LuckyRecord::QueryBuilder
   end
 
   private def prepared_statement_values
-    @wheres.map(&.value)
+    @wheres.compact_map do |sql_clause|
+      sql_clause.value if sql_clause.is_a?(LuckyRecord::Where::ComparativeSqlClause)
+    end
   end
 
   private def next_prepared_statement_placeholder

luckyframework/avram

spec/query_builder_spec.cr
@@ -22,8 +22,9 @@ describe "Avram::QueryBuilder" do
     query = new_query
       .where(Avram::Where::Equal.new(:name, "Paul"))
       .where(Avram::Where::GreaterThan.new(:age, "20"))
+      .where(Avram::Where::Null.new(:nickname))
       .limit(1)
-    query.statement.should eq "SELECT * FROM users WHERE name = $1 AND age > $2 LIMIT 1"
+    query.statement.should eq "SELECT * FROM users WHERE name = $1 AND age > $2 AND nickname IS NULL LIMIT 1"
     query.args.should eq ["Paul", "20"]
   end
 

spec/where_spec.cr
@@ -1,9 +1,15 @@
 require "./spec_helper"
 
 macro should_negate(original_where, expected_negated_where)
-  clause = {{original_where}}.new("column", "value").negated
+  {% if original_where.resolve < Avram::Where::ComparativeSqlClause %}
+    clause = {{original_where}}.new("column", "value").negated
+  {% else %}
+    clause = {{original_where}}.new("column").negated
+  {% end %}
   clause.column.should eq "column"
-  clause.value.should eq "value"
+  {% if original_where.resolve < Avram::Where::ComparativeSqlClause %}
+    clause.value.should eq "value"
+  {% end %}
   clause.should be_a({{expected_negated_where}})
 end
 
@@ -21,5 +27,6 @@ describe Avram::Where do
     should_negate(Avram::Where::NotIlike, Avram::Where::Ilike)
     should_negate(Avram::Where::In, Avram::Where::NotIn)
     should_negate(Avram::Where::NotIn, Avram::Where::In)
+    should_negate(Avram::Where::Null, Avram::Where::NotNull)
   end
 end

src/avram/query_builder.cr
@@ -221,7 +221,13 @@ class Avram::QueryBuilder
 
   private def joined_wheres_queries
     if @wheres.any? || @raw_wheres.any?
-      statements = @wheres.map(&.prepare(next_prepared_statement_placeholder))
+      statements = @wheres.map do |sql_clause|
+        if sql_clause.is_a?(Avram::Where::ComparativeSqlClause)
+          sql_clause.prepare(next_prepared_statement_placeholder)
+        else
+          sql_clause.prepare
+        end
+      end
       statements += @raw_wheres.map(&.to_sql)
 
       "WHERE " + statements.join(" AND ")
@@ -229,7 +235,9 @@ class Avram::QueryBuilder
   end
 
   private def prepared_statement_values
-    @wheres.map(&.value)
+    @wheres.compact_map do |sql_clause|
+      sql_clause.value if sql_clause.is_a?(Avram::Where::ComparativeSqlClause)
+    end
   end
 
   private def next_prepared_statement_placeholder