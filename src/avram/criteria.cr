luckyframework/lucky_record

Disallow `is`/`is_not` from accepting possibly `Nil` parameters
Will now throw an error at compile-time if one writes something like:
```
UserQuery::BaseQuery.nickname.is(nil)
```

The methods `is?`/`is_not?` exist for comparing to possibly `nil`
values.

src/lucky_record/criteria.cr
@@ -16,7 +16,20 @@ class LuckyRecord::Criteria(T, V)
     rows
   end
 
+  def is(_value : Nil)
+    {{ raise "Use `is?` instead of `is` if trying to check against nillable value." }}
+  end
+
   def is(value)
+    is?(value)
+  end
+
+  def is?(_value : Nil)
+    add_clause(LuckyRecord::Where::Null.new(column))
+    rows
+  end
+
+  def is?(value)
     add_clause(LuckyRecord::Where::Equal.new(column, V::Lucky.to_db!(value)))
     rows
   end
@@ -32,6 +45,18 @@ class LuckyRecord::Criteria(T, V)
 
   def is_not(value) : T
     add_clause(LuckyRecord::Where::NotEqual.new(column, V::Lucky.to_db!(value)))
+  end
+
+  def is_not(_value : Nil)
+    {{ raise "Use `is_not?` instead of `is_not` if trying to check against nillable value." }}
+  end
+
+  def is_not?(value) : T
+    add_clause(LuckyRecord::Where::NotEqual.new(column, V::Lucky.to_db!(value)))
+  end
+
+  def is_not?(_value : Nil) : T
+    add_clause(LuckyRecord::Where::NotNull.new(column))
   end
 
   def gt(value) : T

luckyframework/avram

src/avram/criteria.cr
@@ -16,7 +16,20 @@ class Avram::Criteria(T, V)
     rows
   end
 
+  def is(_value : Nil)
+    {{ raise "Use `is?` instead of `is` if trying to check against nillable value." }}
+  end
+
   def is(value)
+    is?(value)
+  end
+
+  def is?(_value : Nil)
+    add_clause(Avram::Where::Null.new(column))
+    rows
+  end
+
+  def is?(value)
     add_clause(Avram::Where::Equal.new(column, V::Lucky.to_db!(value)))
     rows
   end
@@ -32,6 +45,18 @@ class Avram::Criteria(T, V)
 
   def is_not(value) : T
     add_clause(Avram::Where::NotEqual.new(column, V::Lucky.to_db!(value)))
+  end
+
+  def is_not(_value : Nil)
+    {{ raise "Use `is_not?` instead of `is_not` if trying to check against nillable value." }}
+  end
+
+  def is_not?(value) : T
+    add_clause(Avram::Where::NotEqual.new(column, V::Lucky.to_db!(value)))
+  end
+
+  def is_not?(_value : Nil) : T
+    add_clause(Avram::Where::NotNull.new(column))
   end
 
   def gt(value) : T