luckyframework/lucky_record

Invert inheritance tree for WHERE clauses
`IS NULL`/`IS NOT NULL` is kind of a special case, so this makes a bit
more sense.

spec/where_spec.cr
@@ -1,13 +1,13 @@
 require "./spec_helper"
 
 macro should_negate(original_where, expected_negated_where)
-  {% if original_where.resolve < LuckyRecord::Where::ComparativeSqlClause %}
-    clause = {{original_where}}.new("column", "value").negated
-  {% else %}
+  {% if original_where.resolve < LuckyRecord::Where::NullSqlClause %}
     clause = {{original_where}}.new("column").negated
+  {% else %}
+    clause = {{original_where}}.new("column", "value").negated
   {% end %}
   clause.column.should eq "column"
-  {% if original_where.resolve < LuckyRecord::Where::ComparativeSqlClause %}
+  {% unless original_where.resolve < LuckyRecord::Where::NullSqlClause %}
     clause.value.should eq "value"
   {% end %}
   clause.should be_a({{expected_negated_where}})

src/lucky_record/query_builder.cr
@@ -211,10 +211,10 @@ class LuckyRecord::QueryBuilder
   private def joined_wheres_queries
     if @wheres.any? || @raw_wheres.any?
       statements = @wheres.map do |sql_clause|
-        if sql_clause.is_a?(LuckyRecord::Where::ComparativeSqlClause)
-          sql_clause.prepare(next_prepared_statement_placeholder)
-        else
+        if sql_clause.is_a?(LuckyRecord::Where::NullSqlClause)
           sql_clause.prepare
+        else
+          sql_clause.prepare(next_prepared_statement_placeholder)
         end
       end
       statements += @raw_wheres.map(&.to_sql)
@@ -225,7 +225,7 @@ class LuckyRecord::QueryBuilder
 
   private def prepared_statement_values
     @wheres.compact_map do |sql_clause|
-      sql_clause.value if sql_clause.is_a?(LuckyRecord::Where::ComparativeSqlClause)
+      sql_clause.value unless sql_clause.is_a?(LuckyRecord::Where::NullSqlClause)
     end
   end
 

luckyframework/avram

spec/where_spec.cr
@@ -1,13 +1,13 @@
 require "./spec_helper"
 
 macro should_negate(original_where, expected_negated_where)
-  {% if original_where.resolve < Avram::Where::ComparativeSqlClause %}
-    clause = {{original_where}}.new("column", "value").negated
-  {% else %}
+  {% if original_where.resolve < Avram::Where::NullSqlClause %}
     clause = {{original_where}}.new("column").negated
+  {% else %}
+    clause = {{original_where}}.new("column", "value").negated
   {% end %}
   clause.column.should eq "column"
-  {% if original_where.resolve < Avram::Where::ComparativeSqlClause %}
+  {% unless original_where.resolve < Avram::Where::NullSqlClause %}
     clause.value.should eq "value"
   {% end %}
   clause.should be_a({{expected_negated_where}})

src/avram/query_builder.cr
@@ -222,10 +222,10 @@ class Avram::QueryBuilder
   private def joined_wheres_queries
     if @wheres.any? || @raw_wheres.any?
       statements = @wheres.map do |sql_clause|
-        if sql_clause.is_a?(Avram::Where::ComparativeSqlClause)
-          sql_clause.prepare(next_prepared_statement_placeholder)
-        else
+        if sql_clause.is_a?(Avram::Where::NullSqlClause)
           sql_clause.prepare
+        else
+          sql_clause.prepare(next_prepared_statement_placeholder)
         end
       end
       statements += @raw_wheres.map(&.to_sql)
@@ -236,7 +236,7 @@ class Avram::QueryBuilder
 
   private def prepared_statement_values
     @wheres.compact_map do |sql_clause|
-      sql_clause.value if sql_clause.is_a?(Avram::Where::ComparativeSqlClause)
+      sql_clause.value unless sql_clause.is_a?(Avram::Where::NullSqlClause)
     end
   end
 