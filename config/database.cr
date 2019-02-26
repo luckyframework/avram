619d94f6fa51346cef610aa89dad5ddcba1b92c4
Paul Smith <paulcsmith0218@gmail.com>
2019-02-25 20:24:36 -0800

    Add comments

--- config/database.cr
+++ config/database.cr
@@ -1,5 +1,13 @@
 database_name = "avram_dev"
 
+# Adding this struct makes everything work
+#
+# Removing it causes:
+#
+# BUG: trying to upcast Dexter::Formatters::JsonLogFormatter <- Dexter::Formatters::BaseLogFormatter (Exception)
+#   from ???
+#   from ???
+#   from ???
 struct NotEvenUsedAnywhere < Dexter::Formatters::BaseLogFormatter
   def format(data) : Void
     # do nothing
