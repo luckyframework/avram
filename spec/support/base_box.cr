e3b83a361af24998825fea12604f2a4107cbd29c
Edward Loveall <edward@edwardloveall.com>
2019-05-12 17:38:08 -0400

    Add sequence to Avram::Box
    Unique values are a good thing and application developers should use
    them. However, testing with unique values can be a pain since you need
    to create a new value explicitly in tests instead of using a default
    value via a Box.
    
    This makes sequences (some code that I found in the Avram tests)
    included by default in every box. This seems like a thing that users
    shouldn't have to build into every app themselves.

--- spec/support/base_box.cr
+++ spec/support/base_box.cr
@@ -1,6 +1,4 @@
 abstract class BaseBox < Avram::Box
-  SEQUENCES = {} of String => Int32
-
   def self.build
     new.build
   end
@@ -11,11 +9,5 @@ abstract class BaseBox < Avram::Box
 
   def build_pair
     [build, build]
-  end
-
-  def sequence(value : String) : String
-    SEQUENCES[value] ||= 0
-    SEQUENCES[value] += 1
-    "#{value}-#{SEQUENCES[value]}"
   end
 end
