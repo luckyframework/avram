a97c2b7dba359dda775bc587458a3d00571979e9
Paul Smith <paulcsmith0218@gmail.com>
2019-06-29 22:16:07 -0400

    Customize primary key name
    Closes #96

--- spec/support/tagging.cr
+++ spec/support/tagging.cr
@@ -1,5 +1,9 @@
 class Tagging < Avram::Model
+  skip_default_columns
+
   table :taggings do
+    primary_key custom_id : Int64
+    timestamps
     belongs_to tag : Tag
     belongs_to post : Post
   end
