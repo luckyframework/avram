d40d2b21056f3a7a2a611ebbbb0c4b7731edc1ff
Jeremy Woertink <jeremywoertink@gmail.com>
2019-07-02 16:35:08 -0700

    removed some duplication

--- spec/query_associations_spec.cr
+++ spec/query_associations_spec.cr
@@ -39,10 +39,6 @@ describe "Query associations" do
     posts = Post::BaseQuery.new.inner_join_comments.comments(&.body.eq("matching"))
     posts.to_sql[0].should contain "INNER JOIN"
     posts.results.should eq([post_with_matching_comment])
-
-    posts = Post::BaseQuery.new.inner_join_comments.comments(&.body("matching"))
-    posts.to_sql[0].should contain "INNER JOIN"
-    posts.results.should eq([post_with_matching_comment])
   end
 
   it "can query associations with left_join specified" do
@@ -60,10 +56,6 @@ describe "Query associations" do
       .create
 
     posts = Post::BaseQuery.new.left_join_comments.comments(&.body.eq("matching"))
-    posts.to_sql[0].should contain "LEFT JOIN"
-    posts.results.should eq([post_with_matching_comment])
-
-    posts = Post::BaseQuery.new.left_join_comments.comments(&.body("matching"))
     posts.to_sql[0].should contain "LEFT JOIN"
     posts.results.should eq([post_with_matching_comment])
   end
@@ -85,10 +77,6 @@ describe "Query associations" do
     posts = Post::BaseQuery.new.right_join_comments.comments(&.body.eq("matching"))
     posts.to_sql[0].should contain "RIGHT JOIN"
     posts.results.should eq([post_with_matching_comment])
-
-    posts = Post::BaseQuery.new.right_join_comments.comments(&.body("matching"))
-    posts.to_sql[0].should contain "RIGHT JOIN"
-    posts.results.should eq([post_with_matching_comment])
   end
 
   it "can query associations with full_join specified" do
@@ -106,10 +94,6 @@ describe "Query associations" do
       .create
 
     posts = Post::BaseQuery.new.full_join_comments.comments(&.body.eq("matching"))
-    posts.to_sql[0].should contain "FULL JOIN"
-    posts.results.should eq([post_with_matching_comment])
-
-    posts = Post::BaseQuery.new.full_join_comments.comments(&.body("matching"))
     posts.to_sql[0].should contain "FULL JOIN"
     posts.results.should eq([post_with_matching_comment])
   end
