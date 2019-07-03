9167190359cec190506475ec63dbdb5a80913c2f
Jeremy Woertink <jeremywoertink@gmail.com>
2019-07-02 20:04:18 -0700

    adding in right_join_X and full_join_X type safe query methods (#128)
    * adding in right_join_X and full_join_X type safe query methods. fixes #116
    
    * added a few specs for the different types of joins
    
    * removed some duplication
    
    * specs and eyeballs

--- spec/query_associations_spec.cr
+++ spec/query_associations_spec.cr
@@ -8,17 +8,98 @@ describe "Query associations" do
       .body("matching")
       .post_id(post_with_matching_comment.id)
       .create
+
     post_without_matching_comment = PostBox.create
     CommentBox
       .new
       .body("not-matching")
-      .post_id(post_with_matching_comment.id)
+      .post_id(post_without_matching_comment.id)
       .create
 
     posts = Post::BaseQuery.new.join_comments.comments(&.body.eq("matching"))
     posts.results.should eq([post_with_matching_comment])
 
     posts = Post::BaseQuery.new.join_comments.comments(&.body("matching"))
+    posts.results.should eq([post_with_matching_comment])
+  end
+
+  it "can query associations with inner_join specified" do
+    post_with_matching_comment = PostBox.create
+    matching_comment = CommentBox
+      .new
+      .body("matching")
+      .post_id(post_with_matching_comment.id)
+      .create
+
+    post_without_matching_comment = PostBox.create
+    CommentBox
+      .new
+      .body("not-matching")
+      .post_id(post_without_matching_comment.id)
+      .create
+
+    posts = Post::BaseQuery.new.inner_join_comments.comments(&.body.eq("matching"))
+    posts.to_sql[0].should contain "INNER JOIN"
+    posts.results.should eq([post_with_matching_comment])
+  end
+
+  it "can query associations with left_join specified" do
+    post_with_matching_comment = PostBox.create
+    matching_comment = CommentBox
+      .new
+      .body("matching")
+      .post_id(post_with_matching_comment.id)
+      .create
+
+    post_without_matching_comment = PostBox.create
+    CommentBox
+      .new
+      .body("not-matching")
+      .post_id(post_without_matching_comment.id)
+      .create
+
+    posts = Post::BaseQuery.new.left_join_comments.comments(&.body.eq("matching"))
+    posts.to_sql[0].should contain "LEFT JOIN"
+    posts.results.should eq([post_with_matching_comment])
+  end
+
+  it "can query associations with right_join specified" do
+    post_with_matching_comment = PostBox.create
+    matching_comment = CommentBox
+      .new
+      .body("matching")
+      .post_id(post_with_matching_comment.id)
+      .create
+
+    post_without_matching_comment = PostBox.create
+    CommentBox
+      .new
+      .body("not-matching")
+      .post_id(post_without_matching_comment.id)
+      .create
+
+    posts = Post::BaseQuery.new.right_join_comments.comments(&.body.eq("matching"))
+    posts.to_sql[0].should contain "RIGHT JOIN"
+    posts.results.should eq([post_with_matching_comment])
+  end
+
+  it "can query associations with full_join specified" do
+    post_with_matching_comment = PostBox.create
+    matching_comment = CommentBox
+      .new
+      .body("matching")
+      .post_id(post_with_matching_comment.id)
+      .create
+
+    post_without_matching_comment = PostBox.create
+    CommentBox
+      .new
+      .body("not-matching")
+      .post_id(post_without_matching_comment.id)
+      .create
+
+    posts = Post::BaseQuery.new.full_join_comments.comments(&.body.eq("matching"))
+    posts.to_sql[0].should contain "FULL JOIN"
     posts.results.should eq([post_with_matching_comment])
   end
 end
