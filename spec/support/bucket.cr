e560dbc1a28619cd52f33b5e7678be84c8bef146
Jeremy Woertink <jeremywoertink@gmail.com>
2019-07-15 15:17:02 -0700

    formatting

--- spec/support/bucket.cr
+++ spec/support/bucket.cr
@@ -1,12 +1,12 @@
 class Bucket < BaseModel
   table do
-    #column bools : Array(Bool)
-    #column prices : Array(Float64)
-    #column small_numbers : Array(Int16)
-    #column numbers : Array(Int32)
-    #column big_numbers : Array(Int64)
+    # column bools : Array(Bool)
+    # column prices : Array(Float64)
+    # column small_numbers : Array(Int16)
+    # column numbers : Array(Int32)
+    # column big_numbers : Array(Int64)
     column names : Array(String)
-    #column dates : Array(Time)
-    #column ids : Array(UUID)
+    # column dates : Array(Time)
+    # column ids : Array(UUID)
   end
 end
