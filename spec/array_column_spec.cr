46b408d66cbcf1923bcab632386e5173e916b9de
Jeremy Woertink <jeremywoertink@gmail.com>
2019-07-18 13:12:09 -0700

    can never go wrong with a few more specs

---
+++ spec/array_column_spec.cr
@@ -0,0 +1,18 @@
+require "./spec_helper"
+
+private class BucketQuery < Bucket::BaseQuery
+end
+
+describe "Array Columns" do
+  it "fails when passing a single value to an array query" do
+    bucket = BucketBox.new.numbers([1, 2, 3]).create
+    expect_raises(PQ::PQError) do
+      BucketQuery.new.numbers(1).select_count
+    end
+  end
+
+  it "returns no results when passing in a proper query that doesn't match" do
+    bucket = BucketBox.new.numbers([1, 2, 3]).create
+    BucketQuery.new.numbers([1]).select_count.should eq 0
+  end
+end
