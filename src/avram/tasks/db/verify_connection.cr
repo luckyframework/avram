6a3327eb883ad4c306d4b03ca0c9f4c8a9d37668
Jeremy Woertink <jeremywoertink@gmail.com>
2019-07-24 14:26:54 -0700

    adding in new task to test postgres connection

---
+++ src/avram/tasks/db/verify_connection.cr
@@ -0,0 +1,19 @@
+class Db::VerifyConnection < LuckyCli::Task
+  summary "Verify connection to postgres"
+
+  def call
+    begin
+      DB.open(Avram::Migrator::Runner.database_url) do |db|
+      end
+    rescue PQ::ConnectionError | DB::ConnectionRefused
+      puts <<-ERROR
+      Unable to connect to postgres.
+
+      Try this...
+
+        ▸ Check your configuration in config/database.cr
+        ▸ Ensure you can connect locally using `psql`
+      ERROR
+    end
+  end
+end
