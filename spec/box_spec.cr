586dbf38aa95f0c60126e9f4584bbb2a6e761ac6
Jeremy Woertink <jeremywoertink@gmail.com>
2019-08-15 16:05:56 -0700

    update Box.create_pair to now return an array of the instance. Also added method overload for accepting a block. Fixes #199

--- spec/box_spec.cr
+++ spec/box_spec.cr
@@ -1,19 +1,37 @@
 require "./spec_helper"
 
-describe "Sequences" do
-  it "increases a value every time it's called" do
-    BaseBox::SEQUENCES["name"] = 0
+describe Avram::Box do
+  describe "Sequences" do
+    it "increases a value every time it's called" do
+      BaseBox::SEQUENCES["name"] = 0
 
-    tag1 = TagBox.create
-    tag2 = TagBox.create
+      tag1 = TagBox.create
+      tag2 = TagBox.create
 
-    tag1.name.should eq("name-1")
-    tag2.name.should eq("name-2")
+      tag1.name.should eq("name-1")
+      tag2.name.should eq("name-2")
+    end
+
+    it "can be overridden" do
+      tag = TagBox.create(&.name("not-a-sequence"))
+
+      tag.name.should eq("not-a-sequence")
+    end
   end
 
-  it "can be overridden" do
-    tag = TagBox.create(&.name("not-a-sequence"))
+  describe "create_pair" do
+    it "creates 2 tags" do
+      tags = TagBox.create_pair
+      tags.size.should eq 2
+      tags.class.name.should eq "Array(Tag)"
+    end
 
-    tag.name.should eq("not-a-sequence")
+    it "yields the block to both boxes" do
+      tags = TagBox.create_pair do |box|
+        box.name(box.sequence("new-tag"))
+      end
+      tags.first.name.should eq "new-tag-1"
+      tags.last.name.should eq "new-tag-2"
+    end
   end
 end
