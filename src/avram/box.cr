016f527add443c3ae56e781c7cf1ac4082bc2aa2
Jeremy Woertink <jeremywoertink@gmail.com>
2019-08-23 21:16:40 -0700

    Update Box.create_pair to now return an array of the instance.  (#215)
    * update Box.create_pair to now return an array of the instance. Also added method overload for accepting a block. Fixes #199
    
    * added additional box spec. Cleaning up some of the code

--- src/avram/box.cr
+++ src/avram/box.cr
@@ -36,8 +36,35 @@ abstract class Avram::Box
     operation.save!
   end
 
+  # Returns an array with 2 instances of the model from the Box.
+  #
+  # Usage:
+  #
+  # ```crystal
+  # tags = TagBox.create_pair
+  # typeof(tags) # => Array(Tag)
+  # tags.size    # => 2
+  # ```
   def self.create_pair
-    2.times { new.create }
+    create_pair { |box| box }
+  end
+
+  # Similar to `create_pair`, but accepts a block which yields the box instance.
+  #
+  # Both boxes receive the same argument values.
+  #
+  # Usage:
+  #
+  # ```crystal
+  # TagBox.create_pair do |box|
+  #   # set both boxes name to "test"
+  #   box.name("test")
+  # end
+  # ```
+  def self.create_pair
+    [1, 2].map do |n|
+      self.create { |box| yield(box) }
+    end
   end
 
   # Returns a value with a number to use for unique values.
