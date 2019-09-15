dfdb81f3adfea76fe06d4a8d4815968521c5291c
Paul Smith <paulcsmith0218@gmail.com>
2019-09-14 20:22:09 -0400

    Extract schema_enforcer_spec.cr

--- spec/model_spec.cr
+++ spec/model_spec.cr
@@ -14,34 +14,6 @@ private class QueryMe < BaseModel
   end
 end
 
-private class ModelWithMissingButSimilarlyNamedColumn < BaseModel
-  table :users do
-    column mickname : String
-  end
-end
-
-private class ModelWithOptionalAttributeOnRequiredColumn < BaseModel
-  table :users do
-    column name : String?
-  end
-end
-
-private class ModelWithRequiredAttributeOnOptionalColumn < BaseModel
-  table :users do
-    column nickname : String
-  end
-end
-
-private class MissingTable < BaseModel
-  table :definitely_a_missing_table do
-  end
-end
-
-private class MissingButSimilarlyNamedTable < BaseModel
-  table :uusers do
-  end
-end
-
 private class EmptyModelCompilesOk < BaseModel
   table :no_fields do
   end
@@ -145,38 +117,6 @@ describe Avram::Model do
   describe ".column_names" do
     it "returns list of mapped columns" do
       QueryMe.column_names.should eq [:id, :created_at, :updated_at, :email, :age]
-    end
-  end
-
-  describe ".ensure_correct_column_mappings" do
-    it "raises on missing table" do
-      expect_raises Exception, "The table 'definitely_a_missing_table' was not found." do
-        MissingTable.ensure_correct_column_mappings!
-      end
-    end
-
-    it "raises on a missing but similarly named table" do
-      expect_raises Exception, "The table 'uusers' was not found. Did you mean users?" do
-        MissingButSimilarlyNamedTable.ensure_correct_column_mappings!
-      end
-    end
-
-    it "raises on tables with missing columns" do
-      expect_raises Exception, "The table 'users' does not have a 'mickname' column. Did you mean nickname?" do
-        ModelWithMissingButSimilarlyNamedColumn.ensure_correct_column_mappings!
-      end
-    end
-
-    it "raises on nilable column with required columns" do
-      expect_raises Exception, "'name' is marked as nilable (name : String?), but the database column does not allow nils." do
-        ModelWithOptionalAttributeOnRequiredColumn.ensure_correct_column_mappings!
-      end
-    end
-
-    it "raises on required columns with nilable columns" do
-      expect_raises Exception, "'nickname' is marked as required (nickname : String), but the database column allows nils." do
-        ModelWithRequiredAttributeOnOptionalColumn.ensure_correct_column_mappings!
-      end
     end
   end
 
