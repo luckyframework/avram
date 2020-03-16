4962a9aabb8afb3bf6f3f0b2e824d4032bf96902
Paul Smith <paulcsmith0218@gmail.com>
2020-03-16 16:30:12 -0400

    Log failed queries

--- src/avram/pool_statement_logging.cr
+++ src/avram/pool_statement_logging.cr
@@ -3,26 +3,41 @@ module DB
     def exec : ExecResult
       log_query
       statement_with_retry &.exec
+    rescue e : PQ::PQError
+      log_error
+      raise e
     end
 
     def exec(*args_, args : Array? = nil) : ExecResult
       log_query(*args_, args: args)
       statement_with_retry &.exec(*args_, args: args)
+    rescue e : PQ::PQError
+      log_error(*args_, args: args)
+      raise e
     end
 
     def query : ResultSet
       log_query
       statement_with_retry &.query
+    rescue e : PQ::PQError
+      log_error
+      raise e
     end
 
     def query(*args_, args : Array? = nil) : ResultSet
       log_query(*args_, args: args)
       statement_with_retry &.query(*args_, args: args)
+    rescue e : PQ::PQError
+      log_error(*args_, args: args)
+      raise e
     end
 
     def scalar(*args_, args : Array? = nil)
       log_query(*args_, args: args)
       statement_with_retry &.scalar(*args_, args: args)
+    rescue e : PQ::PQError
+      log_error(*args_, args: args)
+      raise e
     end
 
     private def log_query(*args_, args : Array? = nil)
@@ -30,6 +45,14 @@ module DB
         logging_args = EnumerableConcat.build(args_, args)
         logging_args = logging_args.to_a if logging_args.is_a?(EnumerableConcat)
         Avram.logger.log(level, {query: @query, args: logging_args})
+      end
+    end
+
+    private def log_error(*args_, args : Array? = nil)
+      Avram.settings.query_failed_log_level.try do |level|
+        logging_args = EnumerableConcat.build(args_, args)
+        logging_args = logging_args.to_a if logging_args.is_a?(EnumerableConcat)
+        Avram.logger.log(level, {failed_query: @query, args: logging_args})
       end
     end
   end
