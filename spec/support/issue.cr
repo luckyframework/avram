3f461da96bb6709f5f9a002ed551f92ce3dbb9eb
Håkan Nylén <hakan@dun.se>
2020-03-24 22:42:02 +0100

    cleaned format

--- spec/support/issue.cr
+++ spec/support/issue.cr
@@ -2,14 +2,14 @@ class Issue < BaseModel
   COLUMN_SQL = "issues.id, issues.status, issues.role"
 
   avram_enum Status do
-    Opened = 0
-    Closed = 1
+    Opened     = 0
+    Closed     = 1
     Duplicated = 2
   end
 
   avram_enum Role do
-    Issue = 0
-    Bug = 1
+    Issue    = 0
+    Bug      = 1
     Critical = 2
   end
 
