16d0947359e0655fa11a69f467a0f2b67cb88a56
Håkan Nylén <hakan@dun.se>
2020-03-25 19:19:43 +0100

    fixed status without integers

--- spec/support/issue.cr
+++ spec/support/issue.cr
@@ -2,9 +2,9 @@ class Issue < BaseModel
   COLUMN_SQL = "issues.id, issues.status, issues.role"
 
   avram_enum Status do
-    Opened     = 0
-    Closed     = 1
-    Duplicated = 2
+    Opened
+    Closed
+    Duplicated
   end
 
   avram_enum Role do
