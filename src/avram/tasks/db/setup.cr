66d29f3b134c00a980a67159e94ead78b26fa22d
Jeremy Woertink <jeremywoertink@gmail.com>
2020-05-01 14:49:58 -0700

    Adding in new db.setup task to run db.create and db.migrate together. Fixes #359

---
+++ src/avram/tasks/db/setup.cr
@@ -0,0 +1,30 @@
+class Db::Setup < LuckyCli::Task
+  summary "Runs a few tasks for setting up your database"
+
+  def initialize(@quiet : Bool = false)
+  end
+
+  def help_message
+    <<-TEXT
+    #{summary}
+
+    This task will run the following:
+      * db.create   - Create the database from config/database.cr
+      * db.migrate  - Migrate all pending migrations from db/migrations/
+
+
+    Examples:
+
+      lucky db.setup
+      LUCKY_ENV=test lucky db.setup # Setup test database
+
+    TEXT
+  end
+
+  def call
+    Avram::Migrator.run do
+      Avram::Migrator::Runner.create_db(@quiet)
+      Avram::Migrator::Runner.new(@quiet).run_pending_migrations
+    end
+  end
+end
