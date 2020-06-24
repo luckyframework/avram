00c5c44f578ccfc840633c0779ad36534ffd39e0
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-06-23 22:01:03 -0500

    beginning to make basic structure

---
+++ src/avram/factory.cr
@@ -0,0 +1,33 @@
+class Avram::Factory
+  macro register(klass, &block)
+    {% operation = "#{klass.id}::SaveOperation".id %}
+
+    class {{klass}}Factory
+
+      getter operation : {{ operation }} = {{ operation }}.new
+
+      def self.create
+        new.create {{ block }}
+      end
+
+      def create
+        with self yield
+        operation.save!
+      end
+
+      Avram::Factory.setup_instance_methods({{ operation }})
+    end
+  end
+
+  macro setup_instance_methods(operation)
+    {% for attribute in operation.resolve.constant(:COLUMN_ATTRIBUTES) %}
+      def {{ attribute[:name] }}
+        operation.{{ attribute[:name] }}.value = yield
+      end
+    {% end %}
+  end
+
+  macro create(klass)
+    {{klass}}Factory.create
+  end
+end
