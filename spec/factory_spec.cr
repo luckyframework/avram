134ac61eef158931bdcf8d3e1378a62136e72d51
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-06-23 22:49:14 -0500

    Added some sort of trait functionality

--- spec/factory_spec.cr
+++ spec/factory_spec.cr
@@ -5,6 +5,10 @@ Avram::Factory.register(User) do
   available_for_hire { true }
   age { 19 }
   joined_at { Time.utc }
+
+  trait :weirdo do
+    name { "User Test" }
+  end
 end
 
 Avram::Factory.register(SignInCredential) do
@@ -15,5 +19,6 @@ describe Avram::Factory do
   it "works" do
     Avram::Factory.create(User)
     Avram::Factory.create(SignInCredential)
+    pp! Avram::Factory.create(User, :weirdo)
   end
 end
