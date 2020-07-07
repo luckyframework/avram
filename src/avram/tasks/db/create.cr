b79f25bb3c6dcf665749e8eecf49467d2a1ceaaa
Jeremy Woertink <jeremywoertink@gmail.com>
2020-07-07 13:40:50 -0700

    Reverting the db.create task connection test (#419)
    * Removed attempt_to_connect from create task. This caused an issue when your postgres username was the same as your OS username.
    
    * Moving connection error mesage to actual error objects
    
    * trying to figure out this failing spec
    
    * not sure why this spec passes like this
    
    * refactor to remove some unnecessary code in the errors

--- src/avram/tasks/db/create.cr
+++ src/avram/tasks/db/create.cr
@@ -20,26 +20,8 @@ class Db::Create < LuckyCli::Task
   end
 
   def call
-    attempt_to_connect
     Migrator.run do
       Migrator::Runner.create_db(@quiet)
     end
-  rescue
-    uri = URI.parse(connection_url)
-    raise Avram::ConnectionError.new(uri, Avram.settings.database_to_migrate)
-  end
-
-  private def connection_url : String
-    Avram::PostgresURL.build(
-      database: "",
-      hostname: Migrator::Runner.db_host.to_s,
-      username: Migrator::Runner.db_user.to_s,
-      password: Migrator::Runner.db_password.to_s,
-      port: Migrator::Runner.db_port.to_s
-    )
-  end
-
-  private def attempt_to_connect
-    Migrator::Runner.run("psql -q -l #{connection_url}", output: IO::Memory.new)
   end
 end
