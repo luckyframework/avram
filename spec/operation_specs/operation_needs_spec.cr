0a7accfefacaceb1432c1eade09062c44d086cf9
Jeremy Woertink <jeremywoertink@gmail.com>
2020-09-22 10:42:58 -0700

    save operations can use needs properly now

--- spec/operation_specs/operation_needs_spec.cr
+++ spec/operation_specs/operation_needs_spec.cr
@@ -11,12 +11,29 @@ private class OperationWithNeeds < Avram::Operation
   end
 end
 
+private class SavePostWithNeeds < Post::SaveOperation
+  needs info : String
+  needs complex_data : JSON::Any
+  attribute comment_count : Int32 = 2
+
+  before_save do
+    title.value = complex_data["title"].as_s
+  end
+end
+
 describe "Avram::Operation needs" do
-  it "sets up named args on run for needs" do
+  it "sets up named args for needs" do
     OperationWithNeeds.run(tags: ["one", "two"], id: 3) do |operation, value|
       value.should eq "one, two"
       operation.tags.should eq ["one", "two"]
       operation.id.should eq 3
+    end
+
+    data = JSON::Any.new({"title" => JSON::Any.new("A complex title")})
+    SavePostWithNeeds.create(info: "A curious post", complex_data: data) do |operation, record|
+      record.try(&.title).should eq "A complex title"
+      operation.info.should eq "A curious post"
+      operation.complex_data.should be_a JSON::Any
     end
   end
 
@@ -28,6 +45,15 @@ describe "Avram::Operation needs" do
       operation.id.should eq 3
       operation.title.value.should eq "test"
       operation.published.value.should eq true
+    end
+
+    params = Avram::Params.new({"comment_count" => "5"})
+    data = JSON::Any.new({"title" => JSON::Any.new("A complex title")})
+    SavePostWithNeeds.create(params, info: "A curious post", complex_data: data) do |operation, record|
+      record.try(&.title).should eq "A complex title"
+      operation.info.should eq "A curious post"
+      operation.complex_data.should be_a JSON::Any
+      operation.comment_count.value.should eq 5
     end
   end
 end
