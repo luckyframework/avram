32406a5668faaa3cfaf445927bb9fc546057e25c
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-11-12 10:19:20 -0600

    Update association property to assoc_name instead of table_name (#526)

--- src/avram/associations/has_one.cr
+++ src/avram/associations/has_one.cr
@@ -17,7 +17,7 @@ module Avram::Associations::HasOne
     {% foreign_key = foreign_key.id %}
 
     association \
-      table_name: :{{ type_declaration.var }},
+      assoc_name: :{{ type_declaration.var }},
       type: {{ model }},
       foreign_key: {{ foreign_key }},
       relationship_type: :has_one
--- src/avram/base_query_template.cr
+++ src/avram/base_query_template.cr
@@ -70,17 +70,17 @@ class Avram::BaseQueryTemplate
       {% end %}
 
       {% for assoc in associations %}
-        def join_{{ assoc[:table_name] }}
-          inner_join_{{ assoc[:table_name] }}
+        def join_{{ assoc[:assoc_name] }}
+          inner_join_{{ assoc[:assoc_name] }}
         end
 
         {% for join_type in ["Inner", "Left", "Right", "Full"] %}
-          def {{ join_type.downcase.id }}_join_{{ assoc[:table_name] }}
+          def {{ join_type.downcase.id }}_join_{{ assoc[:assoc_name] }}
             {% if assoc[:relationship_type] == :belongs_to %}
               join(
                 Avram::Join::{{ join_type.id }}.new(
                   from: table_name,
-                  to: :{{ assoc[:table_name] }},
+                  to: :{{ assoc[:assoc_name] }},
                   primary_key: {{ assoc[:foreign_key] }},
                   foreign_key: {{ assoc[:type] }}::PRIMARY_KEY_NAME
                 )
@@ -97,13 +97,13 @@ class Avram::BaseQueryTemplate
             {% elsif assoc[:through] %}
               {{ join_type.downcase.id }}_join_{{ assoc[:through].id }}
                 .__yield_where_{{ assoc[:through].id }} do |join_query|
-                  join_query.{{ join_type.downcase.id }}_join_{{ assoc[:table_name] }}
+                  join_query.{{ join_type.downcase.id }}_join_{{ assoc[:assoc_name] }}
                 end
             {% else %}
               join(
                 Avram::Join::{{ join_type.id }}.new(
                   from: table_name,
-                  to: :{{ assoc[:table_name] }},
+                  to: :{{ assoc[:assoc_name] }},
                   foreign_key: {{ assoc[:foreign_key] }},
                   primary_key: primary_key_name
                 )
@@ -113,9 +113,9 @@ class Avram::BaseQueryTemplate
         {% end %}
 
 
-        def where_{{ assoc[:table_name] }}(assoc_query : {{ assoc[:type] }}::BaseQuery, auto_inner_join : Bool = true)
+        def where_{{ assoc[:assoc_name] }}(assoc_query : {{ assoc[:type] }}::BaseQuery, auto_inner_join : Bool = true)
           if auto_inner_join
-            join_{{ assoc[:table_name] }}.merge_query(assoc_query.query)
+            join_{{ assoc[:assoc_name] }}.merge_query(assoc_query.query)
           else
             merge_query(assoc_query.query)
           end
@@ -123,12 +123,12 @@ class Avram::BaseQueryTemplate
 
         # :nodoc:
         # Used internally for has_many through queries
-        def __yield_where_{{ assoc[:table_name] }}
+        def __yield_where_{{ assoc[:assoc_name] }}
           assoc_query = yield {{ assoc[:type] }}::BaseQuery.new
           merge_query(assoc_query.query)
         end
 
-        def {{ assoc[:table_name] }}
+        def {{ assoc[:assoc_name] }}
           \{% raise <<-ERROR
             The methods for querying associations have changed
 
@@ -137,7 +137,7 @@ class Avram::BaseQueryTemplate
 
             Example:
 
-              where_{{ assoc[:table_name] }}({{ assoc[:type] }}Query.new.some_condition)
+              where_{{ assoc[:assoc_name] }}({{ assoc[:type] }}Query.new.some_condition)
             ERROR
           %}
           yield # This is not used. Just there so it works with blocks.
