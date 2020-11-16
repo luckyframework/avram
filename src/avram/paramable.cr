6b232761642bb1ba168ba1e4b7a33168226dda20
Joakim Repomaa <mail@jreinert.com>
2020-11-17 00:37:50 +0100

    add support for many_nested (#53)
    * add support for many_nested
    
    fixes #52
    wait for https://github.com/luckyframework/lucky/pull/681
    
    * Add in the new required param abstractions
    
    Co-authored-by: Jeremy Woertink <jeremywoertink@gmail.com>

--- src/avram/paramable.cr
+++ src/avram/paramable.cr
@@ -1,6 +1,8 @@
 module Avram::Paramable
   abstract def nested?(key : String) : Hash(String, String)
   abstract def nested(key : String) : Hash(String, String)
+  abstract def many_nested?(key : String) : Array(Hash(String, String))
+  abstract def many_nested(key : String) : Array(Hash(String, String))
   abstract def get?(key : String)
   abstract def get(key : String)
 
