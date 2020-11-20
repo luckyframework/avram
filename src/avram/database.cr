luckyframework/avram

commit 361a9e84d4b1d3cf35346f6eb361c9388bcaccf0

Add DatabaseInfo for SchemaEnforcer use (#467)

src/avram/database.cr
@@ -38,6 +38,12 @@ abstract class Avram::Database
     new.delete
   end
 
+  @@database_info : DatabaseInfo?
+
+  def self.database_info : DatabaseInfo
+    @@database_info ||= DatabaseInfo.load(self)
+  end
+
   # Wrap the block in a database transaction
   #
   # ```
@@ -171,67 +177,23 @@ abstract class Avram::Database
     transactions.delete(Fiber.current.object_id)
   end
 
-  def table_names
-    tables_with_schema(excluding: "migrations")
-  end
-
-  def tables_with_schema(excluding : String)
-    select_rows <<-SQL
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema='public'
-    AND table_type='BASE TABLE'
-    AND table_name != '#{excluding}';
-    SQL
-  end
-
-  def select_rows(statement)
-    rows = [] of String
-
-    run do |db|
-      db.query statement do |rs|
-        rs.each do
-          rows << rs.read(String)
-        end
-      end
-    end
-
-    rows
-  end
-
-  def table_columns(table_name)
-    statement = <<-SQL
-    SELECT column_name as name, is_nullable::boolean as nilable
-    FROM information_schema.columns
-    WHERE table_schema = 'public'
-      AND table_name = '#{table_name}'
-    SQL
-
-    run { |db| db.query_all statement, as: TableColumn }
-  end
-
-  class TableColumn
-    DB.mapping({
-      name:    String,
-      nilable: Bool,
-    })
-  end
-
   class DatabaseCleaner
-    private getter database
+    private getter database : Avram::Database
+    private getter database_info : Avram::Database::DatabaseInfo
 
-    def initialize(@database : Avram::Database)
+    def initialize(@database)
+      @database_info = database.class.database_info
     end
 
     def truncate
-      table_names = database.table_names
+      table_names = database_info.table_names
       return if table_names.empty?
       statement = ("TRUNCATE TABLE #{table_names.map { |name| name }.join(", ")} RESTART IDENTITY CASCADE;")
       database.exec statement
     end
 
     def delete
-      table_names = database.table_names
+      table_names = database_info.table_names
       return if table_names.empty?
       table_names.each do |t|
         statement = ("DELETE FROM #{t}")

src/avram/database/table_info.cr
@@ -1,0 +1,35 @@
+module Avram
+  struct Database::TableInfo
+    getter table_name : String
+    getter table_type : String
+    getter table_schema : String
+    getter columns = [] of ColumnInfo
+
+    def initialize(
+      @table_name,
+      @table_type,
+      @table_schema
+    )
+    end
+
+    def table?
+      table_type == "BASE TABLE"
+    end
+
+    def view?
+      table_type == "VIEW"
+    end
+
+    def column?(name : String)
+      column_names.includes?(name)
+    end
+
+    def column_names
+      columns.map(&.column_name)
+    end
+
+    def column(name : String) : ColumnInfo?
+      columns.find(&.column_name.==(name))
+    end
+  end
+end

src/avram/database/column_info.cr
@@ -1,0 +1,24 @@
+module Avram
+  struct Database::ColumnInfo
+    include DB::Serializable
+
+    property table_catalog : String
+    property table_schema : String
+    property table_name : String
+    property table_type : String
+    property column_name : String
+    property is_nullable : String
+
+    def nilable?
+      is_nullable == "YES"
+    end
+
+    def table
+      TableInfo.new(
+        table_name,
+        table_type,
+        table_schema
+      )
+    end
+  end
+end

src/avram/database/database_info.cr
@@ -1,0 +1,46 @@
+module Avram
+  struct Database::DatabaseInfo
+    SQL_QUERY = <<-SQL
+      SELECT columns.table_name,
+            tables.table_type,
+            columns.table_schema,
+            columns.table_catalog,
+            columns.column_name,
+            columns.is_nullable
+      FROM information_schema.columns as columns
+      JOIN information_schema.tables as tables
+        ON tables.table_name = columns.table_name
+        AND tables.table_catalog = columns.table_catalog
+        AND tables.table_schema = columns.table_schema
+      WHERE columns.table_schema='public';
+    SQL
+
+    def self.load(database : Avram::Database.class)
+      column_infos = database.query(SQL_QUERY) { |rs| ColumnInfo.from_rs(rs) }
+
+      grouped = column_infos.group_by(&.table)
+      grouped.each do |table, columns|
+        columns.each { |c| table.columns << c }
+      end
+      table_infos = grouped.keys
+      new(table_infos)
+    end
+
+    property table_infos : Array(TableInfo)
+
+    def initialize(@table_infos)
+    end
+
+    def table?(name : String)
+      table_names.includes?(name)
+    end
+
+    def table_names
+      table_infos.map(&.table_name)
+    end
+
+    def table(name : String) : TableInfo?
+      table_infos.find(&.table_name.==(name))
+    end
+  end
+end

src/avram/schema_enforcer.cr
@@ -12,15 +12,16 @@ module Avram::SchemaEnforcer
         {% end %}
       ]
 
+      database_info = {{ type }}.database.database_info
       EnsureExistingTable.new(
         model_class: {{ type }},
         table_name: :{{table_name}},
-        database: {{ type }}.database
+        database_info: database_info
       ).ensure_exists!
       EnsureMatchingColumns.new(
         model_class: {{ type }},
         table_name: :{{table_name}},
-        database: {{ type }}.database
+        database_info: database_info
       ).ensure_matches! attributes
     end
 
@@ -42,21 +43,19 @@ module Avram::SchemaEnforcer
   end
 
   class EnsureExistingTable
-    private getter table_name, model_class
-    @table_names : Array(String)
+    private getter table_name : Symbol
+    private getter model_class : Avram::Model.class
+    private getter database_info : Database::DatabaseInfo
 
-    def initialize(@model_class : Avram::Model.class,
-                   @table_name : Symbol,
-                   @database : Avram::Database.class)
-      @table_names = @database.new.tables_with_schema(excluding: "migrations")
+    def initialize(@model_class, @table_name, @database_info)
     end
 
     def ensure_exists!
       if table_missing?
-        best_match = Levenshtein::Finder.find @table_name.to_s, @table_names, tolerance: 2
+        best_match = Levenshtein::Finder.find table_name.to_s, database_info.table_names, tolerance: 2
 
         message = String.build do |string|
-          string << "#{@model_class.to_s.colorize.bold} wants to use the '#{table_name.colorize.bold}' table but it is missing.\n"
+          string << "#{model_class.to_s.colorize.bold} wants to use the '#{table_name.colorize.bold}' table but it is missing.\n"
 
           if best_match
             string << <<-TEXT
@@ -104,24 +103,20 @@ module Avram::SchemaEnforcer
     end
 
     private def table_missing?
-      !@table_names.includes?(@table_name.to_s)
+      !database_info.table?(table_name.to_s)
     end
   end
 
   class EnsureMatchingColumns
-    private getter model_class, table_name
-    @columns_map = Hash(String, Bool).new
+    private getter model_class : Avram::Model.class
+    private getter table_name : Symbol
+    private getter table_info : Database::TableInfo
     @missing_columns = [] of String
     @optional_attribute_errors = [] of String
     @required_attribute_errors = [] of String
 
-    def initialize(@model_class : Avram::Model.class,
-                   @table_name : Symbol,
-                   @database : Avram::Database.class)
-      columns = @database.new.table_columns(table_name)
-      columns.each do |column|
-        @columns_map[column.name] = column.nilable
-      end
+    def initialize(@model_class, @table_name, database_info : Avram::Database::DatabaseInfo)
+      @table_info = database_info.table(table_name.to_s).not_nil!
     end
 
     def ensure_matches!(attributes)
@@ -137,15 +132,15 @@ module Avram::SchemaEnforcer
     end
 
     private def check_column_matches(attribute)
-      unless @columns_map.has_key? attribute[:name].to_s
-        @missing_columns << missing_attribute_error(@table_name, @columns_map.keys, attribute)
+      unless column = table_info.column(attribute[:name].to_s)
+        @missing_columns << missing_attribute_error(table_info.table_name, table_info.column_names, attribute)
         return
       end
 
-      if !attribute[:nilable] && @columns_map[attribute[:name].to_s]
-        @required_attribute_errors << required_attribute_error(@table_name, attribute)
-      elsif attribute[:nilable] && !@columns_map[attribute[:name].to_s]
-        @optional_attribute_errors << optional_attribute_error(@table_name, attribute)
+      if !attribute[:nilable] && column.nilable?
+        @required_attribute_errors << required_attribute_error(table_info.table_name, attribute)
+      elsif attribute[:nilable] && !column.nilable?
+        @optional_attribute_errors << optional_attribute_error(table_info.table_name, attribute)
       end
     end
 

commit 529be85066b7a642929f1d5c5c6a027520b2a0fc

Only truncate expected database tables (#541)
We were truncating all table names in DatabaseInfo. This included:
- The migrations table
- Non-tables like views

This bug showed itself while running specs in generated Lucky apps because the database is truncated after every test.

We did not see this in Avram because the specs did not load the database migrations which it now does.

The bug started in https://github.com/luckyframework/avram/commit/361a9e84d4b1d3cf35346f6eb361c9388bcaccf0

src/avram/database.cr
@@ -184,22 +184,27 @@ abstract class Avram::Database
 
   class DatabaseCleaner
     private getter database : Avram::Database
-    private getter database_info : Avram::Database::DatabaseInfo
+    private getter table_names : Array(String)
 
     def initialize(@database)
-      @database_info = database.class.database_info
+      @table_names = database.class
+        .database_info
+        .table_infos
+        .select(&.table?)
+        .reject(&.migrations_table?)
+        .map(&.table_name)
     end
 
     def truncate
-      table_names = database_info.table_names
       return if table_names.empty?
+
       statement = ("TRUNCATE TABLE #{table_names.map { |name| name }.join(", ")} RESTART IDENTITY CASCADE;")
       database.exec statement
     end
 
     def delete
-      table_names = database_info.table_names
       return if table_names.empty?
+
       table_names.each do |t|
         statement = ("DELETE FROM #{t}")
         database.exec statement

src/avram/database/table_info.cr
@@ -31,5 +31,9 @@ module Avram
     def column(name : String) : ColumnInfo?
       columns.find(&.column_name.==(name))
     end
+
+    def migrations_table?
+      table_name == "migrations"
+    end
   end
 end

spec/spec_helper.cr
@@ -7,6 +7,7 @@ require "./support/boxes/base_box"
 require "./support/boxes/**"
 require "./support/**"
 require "../config/*"
+require "../db/migrations/**"
 
 Pulsar.enable_test_mode!
 