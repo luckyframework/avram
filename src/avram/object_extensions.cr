b44b50664e9f245c82545423dac18242fe7b0c5d
Jeremy Woertink <jeremywoertink@gmail.com>
2021-03-31 15:51:32 -0700

    Cleaning up for Crystal 1.0 support (#655)
    * Updating crystal version constraint for allowing through 1.0. Cleaning up CI with matrix
    
    * Moving the blank shard directly in to our Object extensions since there's a version constraint on blank that prevents it from being used with Crystal 1.0 without an update. It's a small shard anyway that's been untouched for several years, so we know the code is solid
    
    * Just start pointing to master branches for now. Next release will lock in to new versions
    
    * Don't need to use alpine for the CI. Updated Dockerfile
    
    * oops, forgot to remove blank. It's no longer needed

--- src/avram/object_extensions.cr
+++ src/avram/object_extensions.cr
@@ -1,4 +1,16 @@
 class Object
+  def blank?
+    if self.responds_to?(:empty?)
+      self.empty?
+    else
+      false
+    end
+  end
+
+  def present?
+    !blank?
+  end
+
   def blank_for_validates_required? : Bool
     blank?
   end
@@ -18,12 +30,28 @@ struct Bool
 end
 
 struct Char
+  def blank?
+    case ord
+    when 9, 0xa, 0xb, 0xc, 0xd, 0x20, 0x85, 0xa0, 0x1680, 0x180e,
+         0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005, 0x2006,
+         0x2007, 0x2008, 0x2009, 0x200a, 0x2028, 0x2029, 0x202f,
+         0x205f, 0x3000
+      true
+    else
+      false
+    end
+  end
+
   def blank_for_validates_required? : Bool
     blank?
   end
 end
 
 struct Nil
+  def blank?
+    true
+  end
+
   def blank_for_validates_required? : Bool
     blank?
   end
