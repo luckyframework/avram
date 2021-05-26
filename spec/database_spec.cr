bf76f9bac7cf4d14d8c89359b75ffdca8f67cc9e
Jeremy Woertink <jeremywoertink@gmail.com>
2021-05-26 10:08:31 -0700

    Using a channel to ensure the listen spec is actually ran and checked

--- spec/database_spec.cr
+++ spec/database_spec.cr
@@ -3,12 +3,15 @@ require "./spec_helper"
 describe Avram::Database do
   describe "listen" do
     it "yields the payload from a notify" do
+      done = Channel(Nil).new
       TestDatabase.listen("dinner_time") do |notification|
         notification.channel.should eq "dinner_time"
         notification.payload.should eq "Tacos"
+        done.send(nil)
       end
 
       TestDatabase.exec("SELECT pg_notify('dinner_time', 'Tacos')")
+      done.receive
     end
   end
 end
