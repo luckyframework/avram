9624465f36006ac7dece3b090ecf02f7d176466e
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2021-07-10 13:49:04 -0400

    Add uniqueness validation providing query (#701)

--- src/avram/database_validations.cr
+++ src/avram/database_validations.cr
@@ -5,8 +5,11 @@ module Avram::DatabaseValidations(T)
   #
   # > This will only work with attributes that correspond to a database column.
   #
+  # Passing a criteria is useful when wanting to change how it compares the value in the database.
+  # For example, making sure that it compares emails that are all downcased to account for different capital letters.
+  #
   # ```
-  # validate_uniqueness_of email
+  # validate_uniqueness_of email, query: UserQuery.new.email.lower
   # ```
   #
   # If there is another email address with the same value, the attribute will be
@@ -21,11 +24,11 @@ module Avram::DatabaseValidations(T)
   # pass and Avram tries to save the record.
   private def validate_uniqueness_of(
     attribute : Avram::Attribute,
-    query : Avram::Criteria,
+    query : Avram::Criteria(T::BaseQuery, _),
     message : String = "is already taken"
   )
     attribute.value.try do |value|
-      if query.eq(value).first?
+      if limit_query(query.eq(value)).first?
         attribute.add_error message
       end
     end
@@ -56,17 +59,42 @@ module Avram::DatabaseValidations(T)
   # ```
   private def validate_uniqueness_of(
     attribute : Avram::Attribute,
-    message : Avram::Attribute::ErrorMessage = "is already taken"
+    query : T::BaseQuery,
+    message : String = "is already taken"
   )
     attribute.value.try do |value|
-      if build_validation_query(attribute.name, value).first?
+      if limit_query(query).where(attribute.name, value).first?
         attribute.add_error message
       end
     end
   end
 
-  private def build_validation_query(column_name, value) : T::BaseQuery
-    query = T::BaseQuery.new.where(column_name, value)
+  # Validates that the given attribute is unique in the database
+  #
+  # > This will only work with attributes that correspond to a database column.
+  #
+  # ```
+  # validate_uniqueness_of name
+  # ```
+  #
+  # If there is another database row with the same name, the attribute will be
+  # marked as invalid.
+  #
+  # Note that you should also add a unique index when creating the table so that
+  # there are no race conditions and you are guaranteed that the value is unique.
+  #
+  # This validation is still useful as it will check for uniqueness along with
+  # all other validations. If you just have the uniqueness constraint in the
+  # database you will not know if there is a collision until all other validations
+  # pass and Avram tries to save the record.
+  private def validate_uniqueness_of(
+    attribute : Avram::Attribute,
+    message : Avram::Attribute::ErrorMessage = "is already taken"
+  )
+    validate_uniqueness_of(attribute: attribute, query: T::BaseQuery.new, message: message)
+  end
+
+  private def limit_query(query)
     record.try(&.id).try do |id|
       query = query.id.not.eq(id)
     end
