666c97f778c8552f30f794ae1ee91b7ca5d61c3d
Jeremy Woertink <jeremywoertink@gmail.com>
2021-08-06 11:00:17 -0700

    Fixing creating enums when the value looks like an int but is actually a string. Fixes #713 (#720)

--- spec/type_extensions/enum_spec.cr
+++ spec/type_extensions/enum_spec.cr
@@ -2,6 +2,10 @@ require "../spec_helper"
 
 include LazyLoadHelpers
 
+private class TestSaveIssue < Issue::SaveOperation
+  permit_columns role, status
+end
+
 describe "models using enums" do
   it "can be created" do
     issue = IssueFactory.create
@@ -34,5 +38,23 @@ describe "models using enums" do
 
     IssueQuery.new.role.select_max.should eq(Issue::Role::Critical)
     IssueQuery.new.role.select_min.should eq(Issue::Role::Issue)
+  end
+
+  it "can use Int values from params" do
+    params = Avram::Params.new({"role" => "3", "status" => "0"})
+    TestSaveIssue.create!(params)
+
+    issue = IssueQuery.new.first
+    issue.role.should eq(Issue::Role::Critical)
+    issue.status.should eq(Issue::Status::Opened)
+  end
+
+  it "can use String values from params" do
+    params = Avram::Params.new({"role" => "Critical", "status" => "Opened"})
+    TestSaveIssue.create!(params)
+
+    issue = IssueQuery.new.first
+    issue.role.should eq(Issue::Role::Critical)
+    issue.status.should eq(Issue::Status::Opened)
   end
 end
