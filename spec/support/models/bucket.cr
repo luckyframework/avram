a2a86c6f206308beb39ac6be13c422ae5613c8dc
Jeremy Woertink <jeremywoertink@gmail.com>
2021-08-06 09:40:18 -0700

    Adding defaults to the array because of a bug that used to exist. This just ensures that bug doesn't come back. Fixes #512

--- spec/support/models/bucket.cr
+++ spec/support/models/bucket.cr
@@ -1,12 +1,12 @@
 class Bucket < BaseModel
   COLUMN_SQL = "buckets.id, buckets.created_at, buckets.updated_at, buckets.bools, buckets.small_numbers, buckets.numbers, buckets.big_numbers, buckets.names, buckets.floaty_numbers, buckets.oody_things"
   table do
-    column bools : Array(Bool)
-    column small_numbers : Array(Int16)
+    column bools : Array(Bool) = [] of Bool
+    column small_numbers : Array(Int16) = [] of Int16
     column numbers : Array(Int32)?
-    column big_numbers : Array(Int64)
-    column names : Array(String)
-    column floaty_numbers : Array(Float64)
-    column oody_things : Array(UUID)
+    column big_numbers : Array(Int64) = [] of Int64
+    column names : Array(String) = [] of String
+    column floaty_numbers : Array(Float64) = [] of Float64
+    column oody_things : Array(UUID) = [] of UUID
   end
 end
