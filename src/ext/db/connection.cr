8d7517198ebbbe99dd9d6292ec397901959b4f64
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-01-05 21:59:09 -0600

    Some cleanup

--- src/ext/db/connection.cr
+++ src/ext/db/connection.cr
@@ -1,11 +1,5 @@
 module DB
   abstract class Connection
     getter stack = [] of DB::Transaction
-
-    def begin_transaction : Transaction
-      return stack.last.begin_transaction if @transaction
-      @transaction = true
-      create_transaction
-    end
   end
 end
