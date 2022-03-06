cc3d0261930d4c9b25140cbf4d29c1d606f273a1
Jeremy Woertink <jeremywoertink@gmail.com>
2022-03-06 15:05:02 -0800

    Using the test db for this spec since the sample db seems to fail in CI

--- spec/avram/migrator/runner_spec.cr
+++ spec/avram/migrator/runner_spec.cr
@@ -3,10 +3,10 @@ require "../../spec_helper"
 describe Avram::Migrator::Runner do
   describe "setup_migration_tracking_table" do
     it "includes a unique index" do
-      Avram.temp_config(database_to_migrate: SampleBackupDatabase) do
+      Avram.temp_config(database_to_migrate: TestDatabase) do
         Avram::Migrator::Runner.setup_migration_tracking_tables
 
-        results = SampleBackupDatabase.query_all("SELECT indexname FROM pg_catalog.pg_indexes WHERE tablename = 'migrations'", as: String)
+        results = TestDatabase.query_all("SELECT indexname FROM pg_catalog.pg_indexes WHERE tablename = 'migrations'", as: String)
         results.should contain("migrations_version_index")
       end
     end
