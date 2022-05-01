3a26731004a06205eb450391b590d0128f60b9dc
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2022-05-01 16:12:13 -0500

    Add optional lucky extensions for avram (#772)
    * Add optional lucky extensions for avram
    
    * Add left behind code
    
    * Add lucky tasks file
    
    require "avram/lucky/tasks/**" didn't work
    
    * Adding this back in from after the merge. It's required so args don't carry across task specs
    
    * point to the real lucky repo
    
    Co-authored-by: Jeremy Woertink <jeremywoertink@gmail.com>

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,16 +1,20 @@
 require "file_utils"
 require "spec"
 require "../src/avram"
+require "../src/lucky"
+require "../src/lucky/tasks"
 require "./support/models/base_model"
 require "./support/models/**"
 require "./support/factories/base_factory"
 require "./support/factories/**"
 require "./support/**"
+require "./lucky/support/**"
 require "../config/*"
 require "../db/migrations/**"
 
 Pulsar.enable_test_mode!
 
+Log.dexter.configure(:none)
 backend = Log::IOBackend.new(STDERR)
 backend.formatter = Dexter::JSONLogFormatter.proc
 Log.builder.bind("avram.*", :error, backend)
@@ -22,6 +26,8 @@ Db::VerifyConnection.new(quiet: true).run_task
 Avram::SpecHelper.use_transactional_specs(TestDatabase)
 
 Spec.before_each do
+  # This clears args between each CLI task spec.
+  ARGV.clear
   # All specs seem to run on the same Fiber,
   # so we set back to NullStore before each spec
   # to ensure queries aren't randomly cached
@@ -38,6 +44,28 @@ SampleBackupDatabase.configure do |settings|
     username: "lucky",
     password: "developer"
   )
+end
+
+Lucky::Session.configure do |settings|
+  settings.key = "_app_session"
+end
+
+Lucky::Server.configure do |settings|
+  settings.secret_key_base = "EPzB4/PA/JZxEhISPr7Ad5X+G73exX+qg8IKFjqwdx0="
+  settings.host = "0.0.0.0"
+  settings.port = 8080
+end
+
+Lucky::RouteHelper.configure do |settings|
+  settings.base_uri = "luckyframework.org"
+end
+
+Lucky::ErrorHandler.configure do |settings|
+  settings.show_debug_output = false
+end
+
+Lucky::ForceSSLHandler.configure do |settings|
+  settings.enabled = true
 end
 
 Habitat.raise_if_missing_settings!
