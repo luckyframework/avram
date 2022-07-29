4b1cb28fa704db693dee8503700be13c73611dc5
Jeremy Woertink <jeremywoertink@gmail.com>
2022-07-29 09:27:18 -0700

    Added the ability to specify Int64 enums for use with Flags. Fixes #825 (#856)

--- spec/support/factories/issue_factory.cr
+++ spec/support/factories/issue_factory.cr
@@ -1,13 +1,15 @@
 class IssueFactory < BaseFactory
   def initialize
     status Issue::Status::Opened
+    permissions Issue::Permissions::Read
   end
 
   def build_model
     Issue.new(
       id: 123_i64,
       status: 0,
-      role: 0
+      role: 0,
+      permissions: 3_i64
     )
   end
 end
