5d4ea66cb7c62cc5177486aea306e1dc9ed21e30
Jeremy Woertink <jeremywoertink@gmail.com>
2023-06-25 09:22:31 -0700

    A little cleanup

--- db/migrations/20230624134902_add_json_array_to_blob.cr
+++ db/migrations/20230624134902_add_json_array_to_blob.cr
@@ -1,7 +1,7 @@
 class AddJsonArrayToBlob::V20230624134902 < Avram::Migrator::Migration::V1
   def migrate
     alter table_for(Blob) do
-      add servers : JSON::Any, default: JSON::Any.new({} of String => JSON::Any)
+      add servers : JSON::Any, default: JSON::Any.new([] of JSON::Any)
     end
   end
 
