ef73cd1c6daa8257e289b9964133c1e09a3079bf
Jeremy Woertink <jeremywoertink@gmail.com>
2023-08-17 12:45:33 -0700

    Using latest lucky_task

--- spec/avram_spec.cr
+++ spec/avram_spec.cr
@@ -1,10 +1,4 @@
 require "./spec_helper"
-require "benchmark"
 
 describe Avram do
-  # Yeah, this is really a thing...
-  it "does not bork Benchmark.memory" do
-    total = Benchmark.memory { Array(Int32).new }
-    total.should eq(32)
-  end
 end
